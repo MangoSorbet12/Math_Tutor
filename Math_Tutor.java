aca2c375cc1babfef1e8cf278c84d027b6403717
Han Grace <2152153@jeffcoschools.us>
2026-09-14 15:12:23 -0600

    Edit1

---
+++ Math_Tutor.java
@@ -0,0 +1,18 @@
+/*
+* Build A Math Tutor
+* Grace Han
+* To be able to make a simple tutoring tool that can solve multiples types of math problems
+*
+* 
+*
+ */
+
+import java.util.Scanner;
+
+public class MathTutor
+{
+    public static void main(String[] args) throws IOException
+    {
+        
+    }
+}
