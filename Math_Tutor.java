dbabdb8e5318dba4fbe9b32f7372f0e5c4d6d05e
Han Grace <2152153@jeffcoschools.us>
2026-09-15 15:10:37 -0600

    I added user print statement

--- Math_Tutor.java
+++ Math_Tutor.java
@@ -13,6 +13,8 @@ public class MathTutor
 {
     public static void main(String[] args) throws IOException
     {
-        
+        System.out.println("Welcome to the Math Tutor Program! This program can solve 6 problems 
+        total sorted into three types: circles, triangles, and lines; to choose your problem
+        type, type (lowercase for all 3) \"c\", \"t\", or \"l\".").
     }
 }
