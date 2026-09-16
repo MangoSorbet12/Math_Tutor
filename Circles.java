34f10b1ef17477abb61ebc54d703021a61398f8d
Han Grace <2152153@jeffcoschools.us>
2026-09-16 08:58:45 -0600

    added circles class

---
+++ Circles.java
@@ -0,0 +1,24 @@
+public class circles
+{
+    private double radius;
+    private boolean isPositive;
+    private double height;
+
+
+    public double circle_Area()
+    {
+        System.out.println("Please input the radius of the circle");
+        radius= input.nextDouble();
+        return Math.PI*radius;
+    }
+
+    public double sphere_Area()
+    {
+        System.out.println("Please input the radius of the base of the sphere");
+        radius= input.nextDouble();
+        System.out.println("Please input the height of the sphere");
+        height= input.nextDouble();
+        return Math.PI*radius*height;
+    }
+}
+
